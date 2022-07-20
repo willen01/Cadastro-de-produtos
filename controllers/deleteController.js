b59af277a8aadc1868db07156008d56cb94729fe
willen <willen.sesantos@gmail.com>
2022-07-20 20:48:34 -0300

    implementa atualização para produtos não pereciveis

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -1,3 +1,5 @@
+
+
 const deleteController = {
     deletarPerecivel: (req, res) => {
 
--- controllers/updateController.js
+++ controllers/updateController.js
@@ -1,5 +1,5 @@
 const PerecivelModel = require("../Models/CadastrarPereciveis");
-const NPerecivelModel = require("../Models/CadastrarPereciveis");
+const NPerecivelModel = require("../Models/CadastrarNPereciveis");
 
 const updateController = {
     atualizarPerecivel: async (req, res) => {
@@ -14,12 +14,23 @@ const updateController = {
         try {
             res.status(200).json({ message: `${nome} atualizado com sucesso` });
         } catch (error) {
-            res.status(400).json({message: `Falha ao atualizar produto ${error}`})
+            res.status(400).json({ message: `Falha ao atualizar produto ${error}` })
         }
     },
 
-    atualizarNPerecivel: (req, res) => {
+    atualizarNPerecivel: async (req, res) => {
+        let nome = req.params.nome;
+        let { quantidade, validade, preco } = req.body;
+        let novosDados = { quantidade, validade, preco };
 
+        const produtoAtualizado = await NPerecivelModel.findOneAndUpdate({nome : nome}, novosDados);
+        if(!produtoAtualizado) return res.status(404).json({message: "Produto não encontrado, tente novamente"});
+
+        try {
+            res.status(200).json({message: `${nome} atualizado com sucesso`});
+        } catch (error) {
+            res.status(400).json({message: `Falha ao atualizar produto ${error}`});
+        }
     }
 }
 
