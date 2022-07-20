efdbdecdbde500bc77a738aefc68393980773461
willen <willen.sesantos@gmail.com>
2022-07-20 16:26:58 -0300

    cria models

---
+++ Models/CadastrarNPereciveis.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const CadastrarNpereciveis = mongoose.Schema({
+    nome: { type: String, required: true },
+    quantidade: { type: Number, required: true },
+    dataEntrada: {type: String, required: true}
+})
+
+module.exports = mongoose.model("NaoPereciveis", CadastrarNpereciveis);
---
+++ Models/CadastrarPereciveis.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const CadastrarPereciveis = mongoose.Schema({
+    nome: { type: String, required: true },
+    quantidade: { type: Number, required: true },
+    validade: { type: String, required: true },
+    preco: { type: String, required: true }
+})
+
+module.exports = mongoose.model("Pereciveis", CadastrarPereciveis);
