99cf3378fbde0c6acbfd6a9f04a8c9779a93fc26
willen <willen.sesantos@gmail.com>
2022-07-20 11:41:41 -0300

    cria rotas

--- server.js
+++ server.js
@@ -6,6 +6,50 @@ app.get("/", (req, res) => {
     res.send("olá mundo")
 })
 
+// buscar/listar produtos
+app.get("/listar/todos", (req, res) => {
+
+})
+
+app.get("listar/pereciveis", (req, res) => {
+
+})
+
+app.get("/listar/npereciveis", (req, res) => {
+
+})
+
+
+//cadastrar produtos
+app.post("/registrar/pereciveis",(req, res) => {
+    
+})
+
+app.post("/registrar/npereciveis",(req, res) => {
+
+})
+
+
+//atualizar produtos
+
+app.put("/atualizar/perecivel/:nome", (req, res) => {
+    
+})
+
+app.put("/atualizar/nperecivel/:nome", (req, res) => {
+
+})
+
+
+//deletar produto
+
+app.delete("/remover/perecivel/:nome", (req, res) => {
+
+})
+
+app.delete("remover/nperecivel/:nome", (req, res) => {
+    
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
