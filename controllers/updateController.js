f53583846e5a436ad85f46215aba36eed51f78f3
willen <willen.sesantos@gmail.com>
2022-07-20 20:34:20 -0300

    implementa atualização de dados no db

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -1,6 +1,21 @@
-const updateController = {
-    atualizarPerecivel: (req, res) => {
+const PerecivelModel = require("../Models/CadastrarPereciveis");
+const NPerecivelModel = require("../Models/CadastrarPereciveis");
 
+const updateController = {
+    atualizarPerecivel: async (req, res) => {
+        let nome = req.params.nome;
+        let { quantidade, validade, preco } = req.body;
+        let novosDados = { quantidade, validade, preco };
+
+        const produtoAtualizado = await PerecivelModel.findOneAndUpdate({ nome: nome }, novosDados);
+
+        if (!produtoAtualizado) return res.status(404).json({ message: "Produto não encontrado, tente novamente" });
+
+        try {
+            res.status(200).json({ message: `${nome} atualizado com sucesso` });
+        } catch (error) {
+            res.status(400).json({message: `Falha ao atualizar produto ${error}`})
+        }
     },
 
     atualizarNPerecivel: (req, res) => {
