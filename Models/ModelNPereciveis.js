89b88ccde34974b53858e27024427a9afbc116ed
willen <willen.sesantos@gmail.com>
2022-07-26 22:36:35 -0300

    adciona propriedade 'perecivel'

--- Models/ModelNPereciveis.js
+++ Models/ModelNPereciveis.js
@@ -4,7 +4,9 @@ const CadastrarNpereciveis = mongoose.Schema({
     nome: { type: String, required: true },
     quantidade: { type: Number, required: true },
     dataEntrada: {type: String, required: true},
-    preco: {type: Number, required:true}
+    preco: {type: Number, required:true},
+    perecivel: {type: Boolean, default: false}
+
 })
 
 module.exports = mongoose.model("NaoPereciveis", CadastrarNpereciveis);
--- Models/ModelPereciveis.js
+++ Models/ModelPereciveis.js
@@ -4,7 +4,8 @@ const CadastrarPereciveis = mongoose.Schema({
     nome: { type: String, required: true },
     quantidade: { type: Number, required: true },
     validade: { type: String, required: true },
-    preco: { type: String, required: true }
+    preco: { type: String, required: true },
+    perecivel: { type: Boolean, default: true }
 })
 
 module.exports = mongoose.model("Pereciveis", CadastrarPereciveis);
